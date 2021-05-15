01b57e798cefaa9052fd367a2d6bb481676e18a2
stearnsbq <stearnsbq@vcu.edu>
2021-05-14 20:59:51 -0400

    rooms progress

--- frontend/app/src/app/rooms/rooms.component.ts
+++ frontend/app/src/app/rooms/rooms.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+import * as Honeycomb from 'honeycomb-grid';
+import * as svgjs from '@svgdotjs/svg.js';
 
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
   styleUrls: ['./rooms.component.scss']
 })
-export class RoomsComponent implements OnInit {
+export class RoomsComponent implements AfterViewInit {
+  @ViewChild("grid") grid: ElementRef;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(){
+
+
+
+
   }
+
+
 
 }
