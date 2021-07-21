9ccef513b4d6aa5d419cea9db7a1ebaf2b66aff3
stearnsbq <stearnsbq@vcu.edu>
2021-07-21 18:18:30 -0400

    messing with kubernetes stuff

--- backend/room-orchestrator/index.ts
+++ backend/room-orchestrator/index.ts
@@ -1,61 +1,70 @@
-import * as k8s from '@kubernetes/client-node';
-import { config } from 'dotenv';
-import { RedisClient } from 'redis';
-import { promisify } from 'util';
+import * as k8s from "@kubernetes/client-node";
+import { config } from "dotenv";
+import { RedisClient } from "redis";
+import { promisify } from "util";
 
 config();
 
 const kubeConfig = new k8s.KubeConfig();
 
-kubeConfig.loadFromCluster();
+kubeConfig.loadFromDefault();
 
 const k8sAPI = kubeConfig.makeApiClient(k8s.CoreV1Api);
 
 const redisClient = new RedisClient({
-    host: process.env.REDIS_BACKEND as string,
-    port: parseInt(process.env.REDIS_PORT as string)
+  host: process.env.REDIS_BACKEND as string,
+  port: parseInt(process.env.REDIS_PORT as string),
 });
 
+function signalHandler(signal: any) {
+  console.log(`*^!@4=> Received signal to terminate: ${signal}`);
 
-function signalHandler(signal: any){
-    console.log(`*^!@4=> Received signal to terminate: ${signal}`)
-
-    redisClient.quit();
+  redisClient.quit();
 }
 
-
-process.on("SIGINT", signalHandler)
-process.on("SIGTERM", signalHandler)
-
-
-
+process.on("SIGINT", signalHandler);
+process.on("SIGTERM", signalHandler);
 
 setInterval(async () => {
+  const poll = await promisify(redisClient.lindex).bind(redisClient)(
+    "roomQueue",
+    0
+  );
 
-    const poll = await promisify(redisClient.lindex).bind(redisClient)("roomQueue", 0);
+  k8sAPI.listNamespacedPod("rooms").then((res) => {
+    console.log(res.body);
+  });
 
+  if (poll) {
+    const { body } = await k8sAPI.listNamespacedPod("rooms");
 
-    if(poll){
+    if (body.items.length <= 100) {
+      const roomToCreate = await promisify(redisClient.lpop).bind(redisClient)(
+        "roomQueue"
+      );
 
-        const {body} = await k8sAPI.listNamespacedPod("rooms")
+      k8sAPI.createNamespacedPod("rooms", {
+        apiVersion: "apps/v1",
+        kind: "Deployment",
+        metadata: {
+          name: "room",
+        },
+        spec: {
+          containers: [
+            {
+              name: "room",
+              image: "stearnsbq/screenhive:room",
+              resources: { limits: { memory: "2048Mi", cpu: "500m" } },
+              ports: [{ containerPort: 8080 }],
+              env: [{name: "STREAMER_JWT", value: "JWT"}, {name: "WS_SERVER", value: "ws://websocket"}]
+            },
+          ],
+        },
+      });
 
-        if(body.items.length <= 100){
-
-            const roomToCreate = await promisify(redisClient.lpop).bind(redisClient)("roomQueue");
-
-            k8sAPI.createNamespacedPod("rooms", {})
-
-
-            // create the pod
-        }
-
+      // create the pod
     }
-    
-}, 1000)
+  }
+}, 1000);
 
-
-
-while (true){}
-
-
-
+// while (true){}
